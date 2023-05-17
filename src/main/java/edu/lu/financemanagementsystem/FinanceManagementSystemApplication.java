9a85fe34f5268c27c5335c6d729e7ca081fdf355
eval <val.karlis@gmail.com>
2023-05-17 20:14:00 +0300

    initial commit

---
+++ src/main/java/edu/lu/financemanagementsystem/FinanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.lu.financemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lu/financemanagementsystem/FinanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lu.financemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
